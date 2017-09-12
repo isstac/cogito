2e8aaa61e26c49b87eeab48a410c3cbeb5ce15ae
Kasper Luckow <ksluckow@gmail.com>
2017-09-11 17:30:45 -0700

    Add jfreechart dep and collect statistics

---
+++ src/main/edu/cmu/sv/isstac/cogito/ml/PredictionStatistics.java
@@ -0,0 +1,7 @@
+package edu.cmu.sv.isstac.cogito.ml;
+
+/**
+ * @author Kasper Luckow
+ */
+public class PredictionStatistics {
+}
---
+++ src/main/edu/cmu/sv/isstac/cogito/visualization/Chart.java
@@ -0,0 +1,7 @@
+package edu.cmu.sv.isstac.cogito.visualization;
+
+/**
+ * @author Kasper Luckow
+ */
+public class Chart {
+}
